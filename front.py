2d8282a883c1594fc7abf8a40a90d57cb0fbebb1
Wisper0098 <61846162+Wisper0098@users.noreply.github.com>
2020-07-29 23:43:11 +0300

    Add files via upload

--- front.py
+++ front.py
@@ -9,6 +9,7 @@ class User_Window():
         self.root = Tk()
         self.root.resizable(width=False, height=False)
         self.name = None
+        self.window = LogIn_Window.window
         self.username_label = Label(self.root, text=f'{LogIn_Window.name}', font='20', fg='black', bg='#d1d3de')
         self.quit_button = Button(self.root, text='Log Out', fg='red', command=self.log_out)
         self.root.geometry('500x250')
@@ -20,18 +21,18 @@ class User_Window():
         self.username_label.pack()
         self.root.mainloop()
 
-    def log_out(self, window):
+    def log_out(self):
         self.root.destroy()
-        window.withdraw()
-
+        self.window.deiconify()
 
 # sadboy123
 # qwerty123   
 
     
 class LogIn_Window(User_Window):
-    def __init__(self, root=None, name='s'):
+    def __init__(self, root=None, window=None, name='s'):
         self.window = Tk()
+        User_Window.window = self.window
         self.window.resizable(width=False, height=False)
         self.window.geometry('500x250')
         self.window.title('Login System 2.0')
@@ -45,6 +46,7 @@ class LogIn_Window(User_Window):
         self.login_button = Button(text='Log in', font='Consolas 13', relief = 'solid', command=self.Log_In)
         self.signUp_button = Button(text='SignUp', font='Consolas 13', relief = 'solid', command=self.Sign_Up)
         self.messageToSignUp = Label(text='Account is not defined\nSign Up, pls!')
+        self.messageToNoneData = Label(text='Login or password is none\n Check this pls!')
         self.text_login.pack()
         self.login.pack()
         self.text_password.pack()
@@ -63,12 +65,14 @@ class LogIn_Window(User_Window):
         
         post = {"user_login": L, "password": P} # Post for database search
         name = post["user_login"] 
+       
         if back.User.LogIn(post, post) == False: 
             return self.messageToSignUp.place(x=185, y=160)
             
         if back.User.LogIn(post, post) == True:
             self.window.withdraw()
             User_Window.name = L
+            User_Window.window = self.window
             return super().__init__()
 
     def Sign_Up(self):
@@ -78,11 +82,17 @@ class LogIn_Window(User_Window):
         ##-----------------------------------
         post = {"user_login": L, "password": P} # Post for database 
         check = back.coll.find_one(post)
+        
+
         if check is None and len(L + P) >= 2: # if login and password not in db, create profile
+            self.window.withdraw()
             back.coll.insert_one(post)
+            User_Window.name = L
+            User_Window.window = self.window
             return super().__init__()
-        else: 
-            pass
+        
+
+LogIn_Window()
 
 
 
