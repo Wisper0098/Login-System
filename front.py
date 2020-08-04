a70fe109b24eedd4d08529e1237b690720dd4eab
Wisper0098 <61846162+Wisper0098@users.noreply.github.com>
2020-08-04 23:08:10 +0300

    Add files via upload

--- front.py
+++ front.py
@@ -1,38 +1,45 @@
 from tkinter import *
+from PIL import Image, ImageTk
+from tkinter import filedialog
 import pymongo
 from pymongo import MongoClient
 import back
 
+#######
+
+class Ticket_Window():
+    def __init__(self):
+        pass 
 
 class User_Window():
     def __init__(self):
         self.root = Tk()
         self.root.resizable(width=False, height=False)
-        self.name = None
-        self.window = LogIn_Window.window
-        self.username_label = Label(self.root, text=f'{LogIn_Window.name}', font='20', fg='black', bg='#d1d3de')
+        self.log_window = LogIn_Window.window #
+        self.username = LogIn_Window.name
+        self.username_label = Label(self.root, font='20', text=self.username, fg='black', bg='#d1d3de')
         self.quit_button = Button(self.root, text='Log Out', fg='red', command=self.log_out)
-        self.root.geometry('500x250')
-        self.root.title(f'Profile: {LogIn_Window.name}')
+        self.createTicket_button = Button(self.root, text='Create ticket', bg='#d1d3de')
+        self.deleteTicket_button = Button(self.root, text='Delete ticket', bg='#d1d3de')
+        self.root.geometry('250x150')
+        self.root.title(f'Profile: {self.username}')
         self.root.wm_attributes('-alpha', 0.9)
         self.root['bg'] = '#d1d3de'
         self.root.iconbitmap(r'D:\Python\my_projects\Login-System\icon.ico')
-        self.quit_button.place(x=440, y=10)
+        self.quit_button.place(x=195, y=123)
+        self.createTicket_button.place(x=10, y=50)
+        self.deleteTicket_button.place(x=10, y=80)
         self.username_label.pack()
-        self.root.mainloop()
-
-    def log_out(self):
-        self.root.destroy()
-        self.window.deiconify()
-
-# sadboy123
-# qwerty123   
-
+        self.root.mainloop()          
     
+    def log_out(self):                
+        self.root.destroy()
+        self.log_window.deiconify()
+
 class LogIn_Window(User_Window):
-    def __init__(self, root=None, window=None, name='s'):
+    def __init__(self, window=None):
         self.window = Tk()
-        User_Window.window = self.window
+        User_Window.window = self.window # set login window for User_Window
         self.window.resizable(width=False, height=False)
         self.window.geometry('500x250')
         self.window.title('Login System 2.0')
@@ -46,7 +53,7 @@ class LogIn_Window(User_Window):
         self.login_button = Button(text='Log in', font='Consolas 13', relief = 'solid', command=self.Log_In)
         self.signUp_button = Button(text='SignUp', font='Consolas 13', relief = 'solid', command=self.Sign_Up)
         self.messageToSignUp = Label(text='Account is not defined\nSign Up, pls!')
-        self.messageToNoneData = Label(text='Login or password is none\n Check this pls!')
+        self.messageToNoneData = Label(text='Login or password is none\n Check this pls!') # will be work soon
         self.text_login.pack()
         self.login.pack()
         self.text_password.pack()
@@ -59,21 +66,17 @@ class LogIn_Window(User_Window):
         L = self.login.get() # Login 
         P = self.password.get() # Password
 
-        # set username 
-        global name
         ##-----------------------------------
         
         post = {"user_login": L, "password": P} # Post for database search
-        name = post["user_login"] 
        
         if back.User.LogIn(post, post) == False: 
             return self.messageToSignUp.place(x=185, y=160)
             
         if back.User.LogIn(post, post) == True:
-            self.window.withdraw()
+            self.window.withdraw() # hide login window
             User_Window.name = L
-            User_Window.window = self.window
-            return super().__init__()
+            User_Window()
 
     def Sign_Up(self):
         L = self.login.get() # Login 
@@ -83,14 +86,15 @@ class LogIn_Window(User_Window):
         post = {"user_login": L, "password": P} # Post for database 
         check = back.coll.find_one(post)
         
-
+        ##
         if check is None and len(L + P) >= 2: # if login and password not in db, create profile
             self.window.withdraw()
             back.coll.insert_one(post)
-            User_Window.name = L
-            User_Window.window = self.window
-            return super().__init__()
-        
+            User_Window.name = L # set user_login
+            User_Window()
+
+# sadboy123
+# qwerty123   
 
 LogIn_Window()
 
